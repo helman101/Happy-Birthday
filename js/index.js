bf5d5cc6ee3004f7885a92a5f963d15b8055fea0
Andres <helmanandresortegon@gmail.com>
2020-06-17 19:11:54 +0200

    progreso

--- js/index.js
+++ js/index.js
@@ -1,28 +1,71 @@
-// !! IMPORTANT README:
+let words = "FELICE CUMPLEANNO!!"; 
+let wordsTwo = "Alla donna più bella, avventurosa e intelligente del mondo!!";
+let paragraph = document.getElementById('head-ini');
+let paragraphTwo = document.getElementById('head-ini-two');
+let paragraphContent = paragraph.innerHTML;
+let paragraphContentTwo = paragraphTwo.innerHTML;
+let counterTwo = 0;
+let counter = 0;
+let buttons = document.querySelectorAll('#btn');
+let body = document.getElementById('photo');
+let mark = false;
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+buttons.forEach((button) =>  {
+  button.addEventListener('click', (e) => {
+    if (mark == false){
+      let div = document.createElement('div');
+      let title = document.createElement('h2');
+      let para = document.createElement('p');
+      let exit = document.createElement('button');
+      div.classList.add('float');
+      exit.classList.add('exit');
+      title.classList.add('title-float');
+      para.classList.add('para-float');
+      
+      switch (button.value) {
+        case "btn-one":
+            title.innerHTML = 'Cariñosa'
+          break;
+      
+        default:
+          break;
+      }
 
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
+      div.appendChild(exit);
+      div.appendChild(title);
+      div.appendChild(para);
+      body.appendChild(div);
 
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
+      exit.addEventListener('click', (e) => {
+        body.removeChild(div);
+      })
+    }
+  })
+})
 
-// Once you have read the above messages, you can delete all comments. 
-"use strict";
+
+setInterval(function() {
+  if (paragraphContent.length < words.length){
+    if (counterTwo < words.length){
+      paragraph.innerHTML += words.slice(counterTwo, counterTwo+1);   
+      paragraphContent = paragraph.innerHTML; 
+    } 
+    counterTwo++; 
+  }   
+}, 150);
+
+setInterval(function() {
+  if (paragraphContent.length == words.length){
+    if (paragraphContentTwo.length < wordsTwo.length){
+      if (counter < wordsTwo.length){
+        paragraphTwo.innerHTML += wordsTwo.slice(counter, counter+1);   
+        paragraphContentTwo = paragraphTwo.innerHTML; 
+      } 
+      counter++; 
+    } 
+  }  
+}, 150);
+
+
+
+
